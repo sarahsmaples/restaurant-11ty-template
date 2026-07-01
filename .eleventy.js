17b94a5efa97279a8139728da3f6793635904c29
Sarah Maples <sarah@tallymeals.com>
2026-07-01 14:16:02 -0500

    Updated Sitemap with live URL

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function (eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(sitemap, {
     sitemap: {
-      hostname: "https://www.example.com", // Replace with your client's URL
+      hostname: "https://nittisnyc.com/", // Replace with your client's URL
     },
   });
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo "Building site..."
 npm run build
 
 echo "Deploying to server..."
-rsync -avz --delete dist/ username@your-server-ip:/var/www/yourdomain.com/
+rsync -avz --delete dist/ rg@104.237.128.61:/var/www/nittisnyc.com/_site/
 
 echo "Deployment complete!"
